e6ebde64c4330763a1b76295bc9ab22d94bfa42f
Redstonerayy <antonpaulr@protonmail.com>
2023-06-21 22:02:37 +0200

    working

---
+++ src/graphics/vulkan_init.cpp
@@ -0,0 +1,111 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <stdexcept>
+#include <vector>
+#include <iostream>
+#include <cstring>
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+    VkDebugUtilsMessageTypeFlagsEXT messageType,
+    const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+    void* pUserData) {
+
+    std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+    return VK_FALSE;
+}
+
+VkInstance vulkan_init(){
+    const std::vector<const char*> validationLayers = {
+        "VK_LAYER_KHRONOS_validation"
+    };
+
+    #ifdef NDEBUG
+        const bool enableValidationLayers = false;
+    #else
+        const bool enableValidationLayers = true;
+    #endif
+
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    for (const char* layerName : validationLayers) {
+        bool layerFound = false;
+
+        for (const auto& layerProperties : availableLayers) {
+            if (strcmp(layerName, layerProperties.layerName) == 0) {
+                layerFound = true;
+                break;
+            }
+        }
+
+        if (!layerFound) {
+            std::cout << "layer not found\n";
+        }
+    }
+
+    if (enableValidationLayers && false) {
+        throw std::runtime_error("validation layers requested, but not available!");
+    }
+
+    uint32_t extensioncount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensioncount, nullptr);
+
+    std::vector<VkExtensionProperties> extensions(extensioncount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensioncount, extensions.data());
+
+    std::cout << "available extensions:\n";
+
+    for (const auto& extension : extensions) {
+        std::cout << '\t' << extension.extensionName << '\n';
+    }
+
+    VkInstance instance;
+
+    VkApplicationInfo appinfo{};
+    appinfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appinfo.pApplicationName = "Minecraft";
+    appinfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appinfo.pEngineName = "Custom Ray Engine";
+    appinfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appinfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createinfo{};
+    createinfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createinfo.pApplicationInfo = &appinfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+    if (enableValidationLayers) {
+        extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    }
+
+    createinfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+    createinfo.ppEnabledExtensionNames = extensions.data();
+    createinfo.enabledLayerCount = 0;
+
+    if (enableValidationLayers) {
+        createinfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+        createinfo.ppEnabledLayerNames = validationLayers.data();
+    } else {
+        createinfo.enabledLayerCount = 0;
+    }
+
+    if (vkCreateInstance(&createinfo, nullptr, &instance) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create instance!");
+    }
+}
+
+void vulkan_terminate(VkInstance instance){
+    vkDestroyInstance(instance, nullptr);
+}
